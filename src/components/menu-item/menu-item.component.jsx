cb60f6a6dedc1cc99c545a3c619bdf884bc8f38d
Caelin Sutch <caelinsutch@gmail.com>
2020-04-18 09:04:00 -0700

    Updated menu items to navigate with router

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { withRouter } from 'react-router-dom';
+
 import "./menu-items.styles.scss";
 
-const MenuItem = ({ title, imageUrl, size }) => (
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
   <div
-    className={`${size} menu-item`}>
+    className={`${size} menu-item`}
+    /*eslint-disable-next-line no-restricted-globals*/
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
     <div className = "background-image"
          style={{
            backgroundImage: `url(${imageUrl}`
@@ -16,4 +21,4 @@ const MenuItem = ({ title, imageUrl, size }) => (
   </div>
 )
 
-export default MenuItem;
+export default withRouter(MenuItem);
